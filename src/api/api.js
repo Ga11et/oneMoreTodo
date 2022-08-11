05bc9818fec04991b6b1c8c2c8bffaea3b86f153
Fyodor <sivkov1998@yandex.ru>
2022-08-11 17:50:48 +0300

    started with layout

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ const prod = 'https://onemoretodo.netlify.app/.netlify/functions'
 
 export const TodoAPI = {
   fetchTodos: async () => {
-    const response = await fetch(dev + '/todos').then(resp => resp.json())
+    const response = await fetch(prod + '/todos').then(resp => resp.json())
     console.log(response)
     return response
   }
